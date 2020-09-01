eaf5e04aadb9509a1bce1fa831b1fc7e52c0c413
Michael Currin <michael.currin@gmail.com>
2020-09-01 10:59:22 +0200

    fix: Update PostView to show appropriate content

--- frontend/static/js/views/PostView.js
+++ frontend/static/js/views/PostView.js
@@ -3,13 +3,14 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
     constructor(params) {
         super(params);
+        this.postID = params.id;
         this.setTitle("Viewing Post");
     }
 
     async getHtml() {
         return `
-            <h1>Posts</h1>
-            <p>You are viewing the posts!</p>
+            <h1>Post</h1>
+            <p>You are viewing post #${this.postID}. No content can be shown here yet.</p>
         `;
     }
 }
